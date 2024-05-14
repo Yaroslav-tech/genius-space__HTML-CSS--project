cca978a318ac3ca255fcf89c63aa20065bf59cc8
Yaroslav-tech <rosk552@gmail.com>
2024-05-14 11:55:24 +0300

    виправлені деякі помилки для перегляду з мобільних

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,23 @@
-const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-btn-open');
-const modalBtnClose = document.querySelector('.modal-btn-close');
+document.addEventListener('DOMContentLoaded', function () {
+  // Отримуємо всі кнопки "Замовити дзвінок"
+  const modalBtns = document.querySelectorAll('.modal-btn-open');
 
-const toggleModal = () => modal.classList.toggle('is-hidden');
+  // Додаємо обробник подій на кожну кнопку
+  modalBtns.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+      // Знаходимо модальне вікно за класом backdrop та змінюємо його стиль, щоб воно стало видимим
+      const modal = document.querySelector('.backdrop');
+      modal.classList.remove('is-hidden');
+    });
+  });
 
-modalBtnOpen.addEventListener('click', toggleModal);
-modalBtnClose.addEventListener('click', toggleModal);
+  // Додаємо обробник подій на кнопку закриття модального вікна
+  const modalCloseBtns = document.querySelectorAll('.modal-btn-close');
+  modalCloseBtns.forEach(function (closeBtn) {
+    closeBtn.addEventListener('click', function () {
+      // Закриваємо модальне вікно, змінюючи стиль
+      const modal = document.querySelector('.backdrop');
+      modal.classList.add('is-hidden');
+    });
+  });
+});
